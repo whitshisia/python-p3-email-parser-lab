4022bc9ce0bbdc93eb816476fde69d6b807e0f30
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-01 19:12:27 +0300

    py

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_regex = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+        emails = email_regex.findall(self.email_string)
+        emails = list(set(emails))
+        emails.sort()
+        return emails
